d7fbb82ca356db7345b2076f3df065aa83189582
Daniel Soli Stensvik <daniel.soli.stensvik@gmail.com>
2022-06-29 10:16:26 +0200

    Added strin extension and used in NFTGrid

---
+++ Lexplorer/Helpers/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Lexplorer.Helpers;
+
+public static class StringExtensions
+{
+    public static string TruncateAtWord(this string input, int length)
+    {
+        if (input == null || input.Length < length)
+            return input!;
+        int iNextSpace = input.LastIndexOf(" ", length, StringComparison.Ordinal);
+        return string.Format("{0}…", input.Substring(0, (iNextSpace > 0) ? iNextSpace : length).Trim());
+    }
+}
